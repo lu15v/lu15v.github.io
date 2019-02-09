d37cd50a01c56827f843a5fd5302dd3f16cd0685
Luis <lu15bytes@gmail.com>
2019-02-09 15:16:08 -0600

    MOD: improved header

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-
+import './header.css';
 
 class Header extends Component {
     render() {
       return (
-        <div className="container">
-          <div className="page-header">
+          <div className="page-header header">
               <h1>{this.props.name}</h1>
+              <div>
+                <p>{this.props.major}</p>      
+                <p>{this.props.desc}</p>    
+              </div>
           </div>
-          <p>{this.props.major}</p>      
-          <p>{this.props.desc}</p>     
-        </div>
+           
+
         
         );
     }
