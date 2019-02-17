186241f82244f663d75ec975dae770cf810f7a31
Luis <lu15bytes@gmail.com>
2019-02-17 16:08:54 -0600

    CRE: projectDetail component

---
+++ src/proj_detail/ProjectDetail.js
@@ -0,0 +1,21 @@
+import React, { Component }  from 'react';
+import './portItem.css';
+
+
+
+class ProjectDetail extends Component {
+
+    render() {
+
+      return (
+        <div>
+       
+        </div>
+        );
+    }
+
+  }
+  
+
+
+  export default ProjectDetail;
