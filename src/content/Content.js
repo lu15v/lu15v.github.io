15021f6d00992fd8037e63f003948ddf35ee11a8
Luis <lu15bytes@gmail.com>
2019-02-16 19:59:05 -0600

    ADDED: more portfolio items

--- src/content/Content.js
+++ src/content/Content.js
@@ -9,10 +9,12 @@ class Content extends Component {
     render() {
       return (
         <div className="wrapper">
-            <PortItem></PortItem>
-            <PortItem></PortItem>
-            <PortItem></PortItem>
-            <PortItem></PortItem>
+            <PortItem>Mars Explorer</PortItem>
+            <PortItem>Connect 4</PortItem>
+            <PortItem>Brick Breaker Game</PortItem>
+            <PortItem>Facebook WS</PortItem>
+            <PortItem>Text Adventure Game</PortItem>
+            <PortItem>Clojure Problems</PortItem>
         </div>
         );
     }
