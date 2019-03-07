2e24852bf00054380c7347b1251f2276d0f6759a
Luis <lu15bytes@gmail.com>
2019-03-06 20:15:35 -0600

    UPDATED: stateful comp for stateless

--- src/social/Social.js
+++ src/social/Social.js
@@ -1,20 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react'
 import  './social.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-class Social extends Component {
-    render() {
-      return (
-          <div className="social">
-             <a href="https://mx.linkedin.com/in/luis-eduardo-ballinas-aguilar-a638a594"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
-             <a href="https://github.com/lu15v"><FontAwesomeIcon icon={['fab', 'github']} /></a>
-             <a href="mailto:luis.ed.ballinas@gmail.com"><FontAwesomeIcon icon={['fas', 'envelope']} /></a>
-             <a href="https://www.facebook.com/XLu15Y"><FontAwesomeIcon icon={['fab', 'facebook']} /></a>
-          </div>
-
-        );
-    }
-  }
-
-
-  export default Social;
+export default function Social() {
+  return (
+    <div className="social">
+      <a href="https://mx.linkedin.com/in/luis-eduardo-ballinas-aguilar-a638a594"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
+      <a href="https://github.com/lu15v"><FontAwesomeIcon icon={['fab', 'github']} /></a>
+      <a href="mailto:luis.ed.ballinas@gmail.com"><FontAwesomeIcon icon={['fas', 'envelope']} /></a>
+      <a href="https://www.facebook.com/XLu15Y"><FontAwesomeIcon icon={['fab', 'facebook']} /></a>
+    </div>
+  )
+}
