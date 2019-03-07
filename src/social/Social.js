0115c0439aa9e5cf4be59838a1696427748caea0
Luis <lu15bytes@gmail.com>
2019-03-06 22:31:49 -0600

    MOD: styles in social component

--- src/social/Social.js
+++ src/social/Social.js
@@ -5,10 +5,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export default function Social() {
   return (
     <div className="social">
-      <a href="https://mx.linkedin.com/in/luis-eduardo-ballinas-aguilar-a638a594"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
-      <a href="https://github.com/lu15v"><FontAwesomeIcon icon={['fab', 'github']} /></a>
-      <a href="mailto:luis.ed.ballinas@gmail.com"><FontAwesomeIcon icon={['fas', 'envelope']} /></a>
-      <a href="https://www.facebook.com/XLu15Y"><FontAwesomeIcon icon={['fab', 'facebook']} /></a>
+      <a id="linkedin"  href="https://mx.linkedin.com/in/luis-eduardo-ballinas-aguilar-a638a594"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
+      <a id="github"  href="https://github.com/lu15v"><FontAwesomeIcon icon={['fab', 'github']} /></a>
+      <a id="email"  href="mailto:luis.ed.ballinas@gmail.com"><FontAwesomeIcon icon={['fas', 'envelope']} /></a>
+      <a id="facebook"  href="https://www.facebook.com/XLu15Y"><FontAwesomeIcon icon={['fab', 'facebook']} /></a>
     </div>
   )
 }
