1a6b16aa390a1aa20c75d217aad8984042f0c1d2
Luis <lu15bytes@gmail.com>
2019-03-10 14:29:54 -0600

    DEL: navbar comp, mod app comp

--- src/App.js
+++ src/App.js
@@ -2,20 +2,30 @@ import React, { Component } from 'react';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 import './App.css';
 import  Header from './components/header/Header';
-import NavBar from './components/navBar/NavBar';
 import Content from './components/content/Content';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faFacebook, faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Detail from './components/detail/Detail';
+
 library.add(faFacebook,faGithub,faLinkedin,faEnvelope); 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header name="Luis Ballinas" major="Software Engineer" desc="Web developer and music lover located in Guadalajara"></Header>
-        <NavBar nLink="4"></NavBar>
-        <Content></Content>
-      </div>
+      <Router>
+          <div className="App">
+            <Header name="Luis Ballinas" major="Software Engineer" desc="Web developer and music lover located in Guadalajara"></Header>
+            <div className="navbar">
+              <a href="https://github.com/lu15v/portfolio/blob/master/src/assets/pdf/Resume.pdf">Resume</a>
+              <Link to={'/'}>Portfolio</Link>
+            </div>
+            <Switch>
+              <Route exact path='/' component={Content}></Route>
+              <Route  path='/item/:item' component={Detail}></Route>
+            </Switch>
+        </div>
+      </Router>
     );
   } 
 }
