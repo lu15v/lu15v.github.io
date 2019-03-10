fb40ffc0d7d1b1e6e6945dca99cb6b86c9efd864
Luis <lu15bytes@gmail.com>
2019-03-10 15:40:13 -0600

    MOD: container to stateless comp

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,30 +1,21 @@
-import React, { Component }  from 'react';
+
+import React from 'react'
 import './content.css';
 import PortItem from '../port_item/PortItem';
-import {Route, Link, BrowserRouter as Router} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
-
-class Content extends Component {
-
-    render() {
-      return (
-        <Router>
-            <div className="wrapper">
-                    <Route><PortItem img="marsExplorer.jpg" path="marsExp">Mars Explorer</PortItem></Route>
-                    <Route><PortItem img="connect4.jpg">Connect 4</PortItem></Route>
-                    <Route><PortItem img="blockBreaker.jpg">Brick Breaker Game</PortItem></Route>
-                    <Route><PortItem img="restFB.jpg">Facebook WS</PortItem></Route>
-                    <Route><PortItem img="wandW.png">Text Adventure Game</PortItem></Route>
-                    <Route><PortItem img="4Clojure.jpg">Clojure Problems</PortItem></Route>
-                    <Route><PortItem img="poker.jpg">Poker </PortItem></Route>
-                    <Route><PortItem img="designPatt.jpg">Design Patterns </PortItem></Route>
-            </div>
-        </Router>
-        );
-    }
-
+  export default function Content() {
+    return (
+        <div className="wrapper">
+            <Link to="/project/marsExp"><PortItem img="marsExplorer.jpg" path="marsExp">Mars Explorer</PortItem></Link>
+            <Link to="/project/connect4"><PortItem img="connect4.jpg">Connect 4</PortItem></Link>
+            <Link to="/project/bbreaker"><PortItem img="blockBreaker.jpg">Brick Breaker Game</PortItem></Link>
+            <Link to="/project/restfb"><PortItem img="restFB.jpg">Facebook WS</PortItem></Link>
+            <Link to="/project/wandw"><PortItem img="wandW.png">Text Adventure Game</PortItem></Link>
+            <Link to="/project/4clojure"><PortItem img="4Clojure.jpg">Clojure Problems</PortItem></Link>
+            <Link to="/project/poker"><PortItem img="poker.jpg">Poker </PortItem></Link>
+            <Link to="/project/designPatt"><PortItem img="designPatt.jpg">Design Patterns </PortItem></Link>
+        </div>
+    )
   }
   
-
-
-  export default Content;
