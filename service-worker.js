da42a9a7513263e46959ac5eef44fec0d4a072b4
Luis <lu15bytes@gmail.com>
2019-03-16 23:50:15 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.201c083c4962e68fe938d752c4e2ca64.js"
+  "/precache-manifest.07eb59ad1c2f8be5bb482d49eb2cabd8.js"
 );
 
 workbox.clientsClaim();
