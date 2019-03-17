67f09d802cdf39d09e94d8b3541ee9ea1487d9f1
Luis <lu15bytes@gmail.com>
2019-03-16 23:47:17 -0600

    Updates

---
+++ precache-manifest.201c083c4962e68fe938d752c4e2ca64.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "55f7d97bd09e793985d8",
+    "url": "/static/css/main.1e06351e.chunk.css"
+  },
+  {
+    "revision": "1ffaee806261364a45ce840bb5111265",
+    "url": "/static/media/wandW.1ffaee80.png"
+  },
+  {
+    "revision": "17d537355810869d732a",
+    "url": "/static/js/1.17d53735.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "55f7d97bd09e793985d8",
+    "url": "/static/js/main.55f7d97b.chunk.js"
+  },
+  {
+    "revision": "5d4b9e5c7b6cf13000b508264a2efce9",
+    "url": "/static/media/4Clojure.5d4b9e5c.jpg"
+  },
+  {
+    "revision": "ddc1eca104b52ac64b94c8448815030f",
+    "url": "/static/media/blockBreaker.ddc1eca1.jpg"
+  },
+  {
+    "revision": "6c8fb6f08339d809e4c6f550608e43e4",
+    "url": "/static/media/connect4.6c8fb6f0.jpg"
+  },
+  {
+    "revision": "e99986ff7b0f4d6a1703c8e111a09e2e",
+    "url": "/static/media/designPatt.e99986ff.jpg"
+  },
+  {
+    "revision": "5f4a4db05ce7cce86b2734b25be152b5",
+    "url": "/static/media/marsExplorer.5f4a4db0.jpg"
+  },
+  {
+    "revision": "3d73a46117d270cba4ce31f176e9ebf1",
+    "url": "/static/media/poker.3d73a461.jpg"
+  },
+  {
+    "revision": "17c7d76e9213d87a09a67c6ab4063224",
+    "url": "/static/media/restFB.17c7d76e.jpg"
+  },
+  {
+    "revision": "17d537355810869d732a",
+    "url": "/static/css/1.5d751686.chunk.css"
+  },
+  {
+    "revision": "95956e1a532e85b558a2ec1f2837a166",
+    "url": "/index.html"
+  }
+];
