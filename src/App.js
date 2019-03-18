b182b5e5dd3c6e6fc7c1a36c48f925d612a74c7f
Luis <lu15bytes@gmail.com>
2019-03-18 00:16:08 -0600

    ADDED: footer to app component

--- src/App.js
+++ src/App.js
@@ -5,26 +5,30 @@ import  Header from './components/header/Header';
 import Content from './components/content/Content';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faFacebook, faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope,faChevronCircleLeft } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope,faChevronCircleLeft, faHeart, faGuitar } from '@fortawesome/free-solid-svg-icons';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Detail from './components/detail/Detail';
 import NavLink from './components/navLink/NavLink';
-library.add(faFacebook,faGithub,faLinkedin,faEnvelope, faChevronCircleLeft); 
+import Footer from './components/footer/Footer';
+library.add(faFacebook,faGithub,faLinkedin,faEnvelope, faChevronCircleLeft, faHeart, faGuitar); 
 class App extends Component {
   render() {
     return (
       <Router>
-          <div className="App">
-            <Header name="Luis Ballinas" major="Software Engineer" desc="Web developer and music lover located in Guadalajara"></Header>
-            <div className="navbar">
-              <a className="link" href="https://github.com/lu15v/lu15v.github.io/blob/dev/src/assets/pdf/Resume.pdf">Resume</a>
-              <NavLink to="/project">Portfolio</NavLink>
-            </div>
-            <Switch>
-              <Route exact path='/project' component={Content}></Route>
-              <Route  path='/project/:item' component={Detail}></Route>
-            </Switch>
-        </div>
+        <div>
+            <div className="App">
+              <Header name="Luis Ballinas" major="Software Engineer" desc="Web developer and music lover located in Guadalajara"></Header>
+              <div className="navbar">
+                <a className="link" href="https://github.com/lu15v/lu15v.github.io/blob/dev/src/assets/pdf/Resume.pdf">Resume</a>
+                <NavLink to="/project">Portfolio</NavLink>
+              </div>
+              <Switch>
+                  <Route exact path='/project' component={Content}></Route>
+                  <Route  path='/project/:item' component={Detail}></Route>
+              </Switch>
+            </div>         
+            <Footer/>
+          </div>
       </Router>
     );
   } 
