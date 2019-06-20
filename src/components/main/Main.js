e8eac935986c1a3679f1755ee24e0277a62ceba4
Luis <lu15bytes@gmail.com>
2019-06-19 22:43:18 -0500

    MOD: abstract description

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './main.css';
+import { timeout } from 'q';
 
 export default class Main extends Component {
 
@@ -7,15 +8,20 @@ export default class Main extends Component {
     return (
       <div className="cont">
         <div className="header">
-        <h3>About me</h3>
-        <hr/>
+          <h3>About me</h3>
+          <hr/>
         </div>
         <main>
-        I'm a software developer with 2+ years of experience, I have worked for international consultancy companies and also international clients. 
-        I love to: contribute, learn, teach and in general be very engaged in every project I'm in. 
-        In my free time, I enjoy learning topics related to web development, playing the ukelele, and developing projects as a freelance and reading some sci-fi books. I want to be part of a team where I can provide my knowledge in projects with high relevance and impact.
+          I'm a full stack developer with 3+ years of experience using state of the art technologies such as: React, Spring, Hibernate, Django, Sql and many more. 
+          I have worked for international consultancy companies and international clients as well.
+
+          I love to: contribute, learn, teach and in general be very engaged in every project I'm in, providing the highest posible quality in every development.
+          In my free time, I enjoy learning topics related to web development, playing the ukelele and developing projects as a freelance.
+          I love to be part of teams where I can learn and provide my knowledge in projects with high relevance and impact.
         </main>
       </div>
     )
   }
 }
+
+
