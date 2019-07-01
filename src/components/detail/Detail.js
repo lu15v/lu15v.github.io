6118c0d7cd431c10bfacf231568aabb1a63f44ba
Luis <lu15bytes@gmail.com>
2019-06-30 21:03:40 -0500

    FEATURE: detail page, added project information details

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -3,20 +3,29 @@ import  './detail.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {Link, withRouter} from 'react-router-dom';
 import {projectDetailInfo} from '../../constants/projectDetailsInfo'
+
 const  Detail = () =>{
   const projName = window.location.pathname.replace('/project/','');
   const projObject = projectDetailInfo[projName];
+
   return (
     <div className="body">
         <Link to="/project"><p className="back"><FontAwesomeIcon icon={['fas', 'chevron-circle-left']} /> Back</p></Link>
-        <main>
+        <main id="project-content">
           <div className="head">
               <h2>{projObject.name}</h2>
           </div>
-          <div className="image">
+          <div className="information">
+            <div className="proj-tech">
+                <h4>Tech</h4>
+            </div>
+            <div className="proj-image">
+              <img  alt="" src={require(`../../assets/images/${projObject.image}`)}></img>
+            </div>
           </div>
+          
           <div className="desc">
-          {projObject.description}
+            {projObject.description}
           </div>
         </main>
     </div>
