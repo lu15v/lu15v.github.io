7dc541c7b68e0d20dcb87cf7d7eba31730ffb4e3
Luis <lu15bytes@gmail.com>
2019-09-02 17:44:06 -0500

    IMPROVED: default page

--- src/components/default_page/defaultPage.jsx
+++ src/components/default_page/defaultPage.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import DeadLink from '../../assets/images/dead_link.jpg';
+import Error from '../../assets/images/error.svg';
 import {Link} from 'react-router-dom';
 import './defaultPage.css';
 
 const defaultPage = () =>(
-    <div className="global-font">
+    <div className="default-page">
         <h1>Great! You kill him</h1>
-        <h3>But don't worry about it we'll fix it for you</h3>
-        <Link to="/" className="linkStyle"><p>Home</p></Link>
-        <Link to="/project" className="linkStyle"><p>Portfolio</p></Link>
-        <div>
-            {<img  alt="" src={DeadLink}></img>}
-        </div>
+        <h3 >But don't worry about it, we'll hide him for you</h3>
+        <Link to="/" className="linkStyle">Home</Link>
+        <Link to="/project" className="linkStyle">Portfolio</Link>
+        <img  alt="" src={Error}></img>
     </div>
 )
 
