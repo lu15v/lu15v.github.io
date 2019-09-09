2f40555db2c5a71f46f3229cbafe203494b7f667
Luis <lu15bytes@gmail.com>
2019-09-08 21:11:39 -0500

    FEATURE: hover items colors random change

--- src/components/port_item/PortItem.jsx
+++ src/components/port_item/PortItem.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState }  from 'react';
 import './portItem.css';
 
 const PortItem = (props) =>{
+    let colors = ['#ffffff ','#ffd700','#00d8ff','#00a1f1','#ffff00','#ffdf8b','#ffde57','#f5dc00','#ffc61e','#ffd430' ];
+    const [selector, setSelector] = useState(0);
     return(
-        <div className="item global-font">
+        <div className="item global-font" onMouseLeave={() => setSelector(0)} onMouseOver={() =>  setSelector(Math.floor(Math.random() * 10)+1)}>
             <img src={require(`../../assets/images/${props.img}`)} alt="" className="image" />
             <div className="opacity">
                 <div className="info"></div>
             </div>
             <div className="tech">
-                <p>{props.children}</p>
+                <p style={{color: colors[selector]}}>{props.children}</p>
             </div>
         </div>
     )
