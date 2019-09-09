9125e663affde0420bc1a41c74b6240a325fb7a3
Luis <lu15bytes@gmail.com>
2019-09-08 21:29:38 -0500

    FEATURE: new page for links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import './navbar.css';
 const navbar = () =>{
   return(
     <div className="navbar">
-        <a className="link" href="https://github.com/lu15v/lu15v.github.io/blob/dev/src/assets/pdf/Resume.pdf">Resume</a>
+        <a target="_blank" rel="noopener noreferrer" className="link" href="https://github.com/lu15v/lu15v.github.io/blob/dev/src/assets/pdf/Resume.pdf">Resume</a>
         <NavLink activeClassName="default active" className="default inactive" exact to="/">About me</NavLink>
         <NavLink  activeClassName="default active" className="default inactive" to="/project">Portfolio</NavLink>
     </div>
--- src/components/social/Social.jsx
+++ src/components/social/Social.jsx
@@ -5,10 +5,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const social = () =>{
   return (
     <div className="social">
-      <a id="linkedin"  href="https://mx.linkedin.com/in/luis-eduardo-ballinas-aguilar-a638a594"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
-      <a id="github"  href="https://github.com/lu15v"><FontAwesomeIcon icon={['fab', 'github']} /></a>
-      <a id="email"  href="mailto:luis.ed.ballinas@gmail.com"><FontAwesomeIcon icon={['fas', 'envelope']} /></a>
-      <a id="facebook"  href="https://www.facebook.com/XLu15Y"><FontAwesomeIcon icon={['fab', 'facebook']} /></a>
+      <a id="linkedin" target="_blank" rel="noopener noreferrer" href="https://mx.linkedin.com/in/luis-eduardo-ballinas-aguilar-a638a594"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
+      <a id="github" target="_blank" rel="noopener noreferrer" href="https://github.com/lu15v"><FontAwesomeIcon icon={['fab', 'github']} /></a>
+      <a id="email" target="_blank" rel="noopener noreferrer" href="mailto:luis.ed.ballinas@gmail.com"><FontAwesomeIcon icon={['fas', 'envelope']} /></a>
+      <a id="facebook" target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/XLu15Y"><FontAwesomeIcon icon={['fab', 'facebook']} /></a>
     </div>
   )
 }
