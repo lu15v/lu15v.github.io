bfc379539f093fbc12c9a3145678b3978757a4ab
lu15v <lu15bytes@gmail.com>
2020-07-07 13:06:01 -0700

    FEATURE: improved defaultPage component, tv lines with flexbox

--- src/components/default_page/defaultPage.jsx
+++ src/components/default_page/defaultPage.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
-import Error from '../../assets/images/error.svg';
-import {Link} from 'react-router-dom';
 import './defaultPage.css';
 
 const defaultPage = () =>(
-    <div className="default-page">
-        <h1>Great! You kill him</h1>
-        <h3 >But don't worry about it, we'll hide him for you</h3>
-        <Link to="/" className="linkStyle">Home</Link>
-        <Link to="/project" className="linkStyle">Portfolio</Link>
-        <img  alt="" src={Error}></img>
+    <div className="flex-container">
+        <div className="column white-column">
+        </div>
+        <div className="column yellow-column">
+        </div>
+        <div className="column blue-column">
+        </div>
+        <div className="column green-column">
+        </div>
+        <div className="column salmon-column">
+        </div>
+        <div className="column orange-column">
+        </div>
+        <div className="column navy-column">
+        </div>
+        <div className="column2 dark-blue-column">
+        </div>
+        <div className="column2 light-black-column">
+        </div>
+        <div className="column2 salmon-column">
+        </div>
+        <div className="column2 light-black-column">
+        </div>
+        <div className="column2 green-water-column"> 
+        </div>
+        <div className="column2 light-black-column">  
+        </div>
+        <div className="column2 grey-column">
+        </div>
+        <div className="column3 king-blue-column">
+        </div>
+        <div className="column3 light-grey-column">
+        </div>
+        <div className="column3 king-blue-column">
+        </div>
+        <div className="column3-last light-black-column">  
+        </div>
     </div>
 )
 
