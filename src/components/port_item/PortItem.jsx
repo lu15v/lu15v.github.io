2bfbec9f6e560d00f8c55b5e784af03d084a7ae7
lu15v <lu15bytes@gmail.com>
2020-07-09 00:09:26 -0700

    CHORE: removed useless console log

--- src/components/port_item/PortItem.jsx
+++ src/components/port_item/PortItem.jsx
@@ -5,7 +5,6 @@ const PortItem = (props) =>{
     let colors = ['#fff', '#ffd700','#00d8ff','#2eff2e','#cb2eff','#ff2eff','#7AA5A1','#ab9df2', '#912626','#C48787','#fc9867','#fffcdc','#69B569','#348bc3','#ff1493','#C15FA8',
                  '#b8c54c','#c37d46','#f0c865','#aa1aed','#d52c2d','#cfef91','#fccd1f','#cc82dd','#b8ae35','#20e379','#A3A7D4','#f3b948' ];
     const [selector, setSelector] = useState(0);
-    console.log(selector)
     return(
         <div className="item global-font" onMouseLeave={() => setSelector(0)} onMouseOver={() =>  setSelector(Math.floor(Math.random() * 28)+ 1)}>
             <img src={require(`../../assets/images/${props.img}`)} alt="" className="image" />
