3f135511b19a98a5d5669149258d6785c30fc931
lu15v <lu15bytes@gmail.com>
2020-08-12 21:34:10 -0700

    CHORE: proj sorting according to development topic

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -12,11 +12,11 @@ import PageContentContainer from '../pageContentContainer/pageContentContainer';
                 <Link to="/project/photoSite"><PortItem img="photoSite.png">Photograph website </PortItem></Link>
                 <Link to="/project/burger"><PortItem img="burgerSeller.png" path="burger">Burger Price Estimator</PortItem></Link>
                 <Link to="/project/setsStore"><PortItem img="setsStore.png">Sets Store</PortItem></Link>
+                <Link to="/project/wandw"><PortItem img="wandW.png">Text Adventure Game</PortItem></Link>
+                <Link to="/project/restfb"><PortItem img="restFB.jpg">Facebook WS</PortItem></Link>
                 <Link to="/project/marsExp"><PortItem img="marsExplorer.jpg" path="marsExp">Mars Explorer</PortItem></Link>
                 <Link to="/project/connect4"><PortItem img="connect4.jpg">Connect 4</PortItem></Link>
                 <Link to="/project/bbreaker"><PortItem img="blockBreaker.jpg">Brick Breaker Game</PortItem></Link>
-                <Link to="/project/restfb"><PortItem img="restFB.jpg">Facebook WS</PortItem></Link>
-                <Link to="/project/wandw"><PortItem img="wandW.png">Text Adventure Game</PortItem></Link>
                 <Link to="/project/fourClojure"><PortItem img="4Clojure.jpg">Clojure Problems</PortItem></Link>
                 {/*<Link to="/project/poker"><PortItem img="poker.jpg">Poker </PortItem></Link>*/}
                 <Link to="/project/designPatt"><PortItem img="designPatt.jpg">Design Patterns </PortItem></Link>
