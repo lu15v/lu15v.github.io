2fa3a7d9aa7484574ff1b664fa58be83e74a53db
lu15v <lu15bytes@gmail.com>
2020-08-12 21:32:43 -0700

    FIX: deprecated properties

--- src/constants/projectDetailsInfo.js
+++ src/constants/projectDetailsInfo.js
@@ -4,83 +4,95 @@ export const projectDetailInfo = {
         image: 'theRosa.png',
         description: 'Restaurant website based. The site was developed using: HTML5, CSS3 & JS (for some animations) with the "Mobile first" design strategy. The project excels the animations and smooth transitions while the user gets more into it.',
         technologies: '',
-        github: 'https://github.com/lu15v/The-Rosa-Restaurant'
+        github: 'https://github.com/lu15v/The-Rosa-Restaurant',
+        demo: 'https://the-rosa-7454b.firebaseapp.com/'
     },
     photoSite:{
         name: 'Photography website',
         image: 'photoSiteMain.png',
         description: 'Website developed and designed with Sketch and React. The Application makes use of Material UI Design framework. It shows a gallery and allows you to see a preview of every image, the picture preview is inspired on the labels found next to the artworks.',
         technologies: '',
-        github: 'https://github.com/lu15v/photo-site'
+        github: 'https://github.com/lu15v/photo-site',
+        demo: 'https://photo-site-51706.firebaseapp.com/'
     },
     burger:{
         name: 'Burger Price Estimator',
         image: 'burgerSeller.png',
         description: 'Burger Price Estimator, developed in React. The application renders/removes every single component when the user adds/deletes ingredientes and estimates the total price of the burger',
         technologies: '',
-        github: 'https://github.com/lu15v/Burger'
+        github: 'https://github.com/lu15v/Burger',
+        demo: 'https://my-burger-2a01a.firebaseapp.com/'
     },
     setsStore:{
         name: 'Sets Store',
         image: 'setsStore.png',
         description: 'Sets store, developed in React usign "React-bootstrap". The site shows to the user a slider showing the new arrivals, and all the sets available showing in detail the: price, name, description, and artist. Also the website has implemented a cart and a login page (full implementation still pending) ',
         technologies: '',
-        github: 'https://github.com/lu15v/sets-store'
+        github: 'https://github.com/lu15v/sets-store',
+        demo: 'https://sets-store.firebaseapp.com/'
     },
     marsExp :{
         name: 'Mars Explorer',
         image: "marsExplorer.jpg",
         description: 'Simulation of the Mars explorer, developed with: Lua and Love2d for the AI class. Using Reactive Agents with and without operative mode',
         technologies: ['Lua', 'Love2d'],
-        github: 'https://github.com/lu15v/Mars-Explorer-'
+        github: 'https://github.com/lu15v/Mars-Explorer-',
+        demo: ''
     },
     connect4 :{
         name: 'Connect 4',
         image: 'connect4.jpg',
         description: 'Implementation of the classic game Connect 4. Developed with: Lua and Love2d for the AI class. Using simulated annealing',
         technologies: ['Lua', 'Love2d'],
-        github: 'https://github.com/lu15v/Connect-4'
+        github: 'https://github.com/lu15v/Connect-4',
+        demo: ''
     },
     bbreaker :{
         name: 'Block Breaker Game',
         image: 'blockBreaker.jpg',
         description: 'Implementation of the classic game block breaker, with some new features such as: special power for the bar. Game developed with: C++, OpenGL and SDL.',
         technologies: ['C++', 'OpenGL', 'SDL'],
-        github: 'https://github.com/lu15v/Game-OpenGL-and-SDL'
+        github: 'https://github.com/lu15v/Game-OpenGL-and-SDL',
+        demo: ''
     },
     restfb :{
         name: 'Rest Facebook API',
         image: 'restFB.jpg',
         description: 'A Web Service using: RestFb 2.8.0, JDK 8 and Glassfish 4.1.2. Allowing interoperability with older versions of Glassfish and JDK. (Versions not compatible with TLSv2)',
         technologies: '',
-        github: 'https://github.com/lu15v/Facebook-Web-Service'
+        github: 'https://github.com/lu15v/Facebook-Web-Service',
+        demo: ''
     },
     wandw :{
         name: 'Text Adventure Game',
         image: 'wandW.png',
         description: 'Implementation of an improved version of “Werewolves and Wanderer” text adventure game as explained by Tim Hartnell in his book called: Creating Adventure Games On Your Computer.',
         technologies: ['Javascript', 'SQLite', 'Sinatra'],
-        github: 'https://github.com/lu15v/Text-Adventure-Game'
+        github: 'https://github.com/lu15v/Text-Adventure-Game',
+        demo: ''
     },
     fourClojure :{
         name: '4 Clojure',
         image: '4Clojure.jpg',
         description: 'A Set of classical funcional problems developed Using Clojure from the famous web page 4Clojure.com. 4Clojure is a web resource that helps developers to learn Clojure through interactive problems.',
         technologies: '',
-        github: 'https://github.com/lu15v/TC2006'
+        github: 'https://github.com/lu15v/TC2006',
+        demo: ''
     },
     poker :{
         name: 'Poker',
         image: '../../assets/images',
         description: '',
         technologies: '',
-        github: ''
+        github: '',
+        demo: ''
     },
     designPatt :{
         name: 'Design Patterns',
         image: 'designPatt.jpg',
         description: 'Custom implemetations of design patterns based on the book: "Design Patterns in Ruby".',
         technologies: '',
-        github: 'https://github.com/lu15v/TC3049'
+        github: 'https://github.com/lu15v/TC3049',
+        demo: ''
     },
 };
