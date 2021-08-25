1a172e70a00085a1c541d65b5ee0171fcdfa7ead
lu15v <lu15bytes@gmail.com>
2021-08-25 00:04:16 -0500

    MOD: Resume updated, and about me desc

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -10,16 +10,16 @@ const main = () => {
           <hr/>
         </div>
         <main>
-          <p>I'm a full-stack developer specialized on front-end development with 3+ years of experience.
-              During all this time I've been working for international companies and clients, using such a great variety of state of the art technologies such as 
-              React, Spring, Rails, Nodejs, Firebase. being React my main area of expertise.</p>
+          <p>I'm a passionate UI developer (with some back end development background) with 3+ years of experience.
+              During all this time I've been working for international companies such as: USAA, Microsoft and Scotiabank, using a great variety of state of the art technologies such as 
+              React, Spring, Rails, Nodejs. being React my main area of expertise.</p>
           
           <p>I love to: contribute, learn, teach and in general be very engaged 
               in every project I'm in, providing the highest possible quality in every development.</p>
 
-          <p>In my free time, I enjoy: learning topics related to web development, playing the ukulele, watching series, writing, reading novels, and developing projects as a freelance. </p>
+          <p>In my free time, I enjoy: learning topics related to web development, playing the ukulele, watching series, writing, reading novels, playing video games and developing custom websites. </p>
           
-          <p>I love and I'm looking forward to be part of teams where I can learn and provide my knowledge in projects with high relevance and impact.</p>
+          <p>I love and I'm looking forward to being part of teams where I can learn and share my knowledge in projects with high relevance and impact.</p>
         </main>
       </div>
     )
